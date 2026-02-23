6cc74c2afd609be1a66e53c801f841bb00847f27
Anuhas20091119 <yasiruanuhas579@gmail.com>
2026-02-24 01:01:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/59n13Fys/IMG-20260220-WA0034.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 ANUWA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
